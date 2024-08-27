088811cbd5467f7437f7574e5fa37f9fe60aded2
Serban Untu <133024956+SerbanUntu@users.noreply.github.com>
2024-08-27 20:14:27 +0200

    feat: file reading, basic error handling

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.err.println(
+                    "Please provide a path to the target .bf file.\nThe correct format is `java Main <path-to-file>`.");
+            return;
+        }
+        Path pathToFile = Path.of(args[0]);
+        try {
+            String content = Files.readString(pathToFile);
+            if (!args[0].endsWith(".bf")) {
+                System.err.println("The file at path \"" + args[0]
+                        + "\" is not a valid Brainf*ck file.\nOnly files ending with .bf are valid targets for the interpreter.");
+                return;
+            }
+            System.out.println(content);
+        } catch (IOException err) {
+            System.err.println("The file at path \"" + args[0]
+                    + "\" does not exist.\nPlease enter a valid path to a target .bf file.");
+        }
+    }
+}
